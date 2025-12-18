9a6b1283d85123ae6662383b507252b0a8993f5a
ZahwaAnritia <anritia15@gmail.com>
2025-12-18 16:11:08 +0700

    Menambahkan endpoint post insertTM

--- app/src/main/java/com/example/questapi_116/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/questapi_116/apiservice/ServiceApiSiswa.kt
@@ -9,7 +9,8 @@ interface ServiceApiSiswa {
     @GET( value="bacaTeman.php")
     suspend fun getSiswa(): List<DataSiswa>
 
-
+    @POST(value="insertTM.php")
+    suspend fun postSiswa (@Body dataSiswa: DataSiswa): retrofit2.Response<Void>
 
 
 }
