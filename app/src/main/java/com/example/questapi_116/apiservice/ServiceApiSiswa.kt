757e68817e7bf80b5ffe8619cb424e145c9034f1
ZahwaAnritia <anritia15@gmail.com>
2025-12-18 20:07:07 +0700

    memperbaiki error

--- app/src/main/java/com/example/questapi_116/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/questapi_116/apiservice/ServiceApiSiswa.kt
@@ -6,11 +6,18 @@ import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface ServiceApiSiswa {
-    @GET( value="bacaTeman.php")
+    @GET("bacaTeman.php")
     suspend fun getSiswa(): List<DataSiswa>
 
-    @POST(value="insertTM.php")
-    suspend fun postSiswa (@Body dataSiswa: DataSiswa): retrofit2.Response<Void>
+    @POST("insertTM.php")
+    suspend fun postSiswa(@Body dataSiswa: DataSiswa):retrofit2.Response<Void>
 
+//    @GET("bacaTeman.php/{id}")
+//    suspend fun getSatuSiswa(@Query("id") id: Int): DataSiswa
 
+//    @PUT("editTM.php/{id}")
+//    suspend fun  editSatuSiswa(@Query("id") id:Int,@Body dataSiswa: DataSiswa):retrofit2.Response<Void>
+
+//    @DELETE("deleteTM.php/{id}")
+//    suspend fun hapusSatuSiswa(@Query("id") id: Int):retrofit2.Response<Void>
 }
