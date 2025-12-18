50ca466e14c0c54809b2ba72cc45a0ff76d6b164
ZahwaAnritia <anritia15@gmail.com>
2025-12-18 20:06:04 +0700

    menambahkan DestinasiEntry.kt

---
+++ app/src/main/java/com/example/questapi_116/uicontroller/route/DestinasiEntry.kt
@@ -0,0 +1,9 @@
+package com.example.questapi_116.uicontroller.route
+
+
+import com.example.questapi_116.R
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
