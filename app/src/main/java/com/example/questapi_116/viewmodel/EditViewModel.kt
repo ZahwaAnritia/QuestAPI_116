424e21610e9dfcf6e662ea04d3fcd42bafcb843d
ZahwaAnritia <anritia15@gmail.com>
2025-12-25 16:09:23 +0700

    memperbaiki error edit view model

--- app/src/main/java/com/example/questapi_116/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/questapi_116/viewmodel/EditViewModel.kt
@@ -4,21 +4,21 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.example.questapi_116.modeldata.DetailSiswa
 import com.example.questapi_116.modeldata.UIStateSiswa
-import com.example.questapi_116.modeldata.toDataSiswa
+import com.example.questapi_116.modeldata.toUiStateSiswa
 import com.example.questapi_116.repositori.RepositoryDataSiswa
-import kotlinx.coroutines.launch
 import retrofit2.Response
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.questapi_116.modeldata.toDataSiswa
+import com.example.questapi_116.uicontroller.route.DestinasiDetail
+import kotlinx.coroutines.launch
 
-
-class EditViewModel(savedStateHandle: SavedStateHandle, private val repositoryDataSiswa:
-RepositoryDataSiswa
-): ViewModel() {
+class EditViewModel(savedStateHandle: SavedStateHandle, private val repositoryDataSiswa: RepositoryDataSiswa): ViewModel(){
     var uiStateSiswa by mutableStateOf(UIStateSiswa())
         private set
+
     private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
     init {
         viewModelScope.launch {
@@ -26,19 +26,20 @@ RepositoryDataSiswa
                 .toUiStateSiswa(true)
         }
     }
-    fun updateUiState(detailSiswa: DetailSiswa) {
-        uiStateSiswa =
-            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
+
+    fun updateUiState(detailSiswa: DetailSiswa){
+        uiStateSiswa = UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
     }
-    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa ): Boolean {
-        return with(uiState) {
+
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+        return with(uiState){
             nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
         }
     }
+
     suspend fun editSatuSiswa(){
         if (validasiInput(uiStateSiswa.detailSiswa)){
-            val call: Response<Void> = repositoryDataSiswa.editSatuSiswa(idSiswa,uiStateSiswa
-                .detailSiswa.toDataSiswa())
+            val call: Response<Void> = repositoryDataSiswa.editSatuSiswa(idSiswa,uiStateSiswa.detailSiswa.toDataSiswa())
 
             if (call.isSuccessful){
                 println("Update Sukses : ${call.message()}")
@@ -48,5 +49,3 @@ RepositoryDataSiswa
         }
     }
 }
-
-
