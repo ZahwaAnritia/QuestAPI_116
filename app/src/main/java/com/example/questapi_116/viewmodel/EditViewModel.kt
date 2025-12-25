7a0a807c0227162ed8cb5f20f072b6ac75e78344
ZahwaAnritia <anritia15@gmail.com>
2025-12-25 15:44:26 +0700

    menambahkan EditViewModel untuk edit data siswa

--- app/src/main/java/com/example/questapi_116/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/questapi_116/viewmodel/EditViewModel.kt
@@ -1,2 +1,14 @@
 package com.example.questapi_116.viewmodel
 
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.questapi_116.modeldata.UIStateSiswa
+import com.example.questapi_116.repositori.RepositoryDataSiswa
+
+
+class EditViewModel(savedStateHandle: SavedStateHandle, private val repositoryDataSiswa:
+RepositoryDataSiswa
+): ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
