9e7a8c4051c3cb2b8b4b1291455f937a2bcb0c57
ZahwaAnritia <anritia15@gmail.com>
2025-12-25 15:40:56 +0700

    menambahkan getSatuSiswa dengan StatusUIDetail

--- app/src/main/java/com/example/questapi_116/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questapi_116/viewmodel/DetailViewModel.kt
@@ -1,11 +1,17 @@
 package com.example.questapi_116.viewmodel
 
 
+import android.net.http.HttpException
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.questapi_116.modeldata.DataSiswa
 import com.example.questapi_116.repositori.RepositoryDataSiswa
+import kotlinx.coroutines.launch
+import java.io.IOException
 
 
 sealed interface StatusUIDetail {
@@ -24,4 +30,18 @@ RepositoryDataSiswa
 
     init {
         getSatuSiswa()
+    }
+    fun getSatuSiswa(){
+        viewModelScope.launch {
+            statusUIDetail = StatusUIDetail.Loading
+            statusUIDetail = try {
+                StatusUIDetail.Success(satusiswa = repositoryDataSiswa.getSatuSiswa(idSiswa))
+            }
+            catch (e: IOException){
+                StatusUIDetail.Error
+            }
+            catch (e: HttpException){
+                StatusUIDetail.Error
+            }
+        }
     }
