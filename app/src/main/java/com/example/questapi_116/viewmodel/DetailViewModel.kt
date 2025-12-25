7812a20f3d383b86da9482bb3e5ba7feae30ffb4
ZahwaAnritia <anritia15@gmail.com>
2025-12-25 15:37:36 +0700

    menambahkan StatusUIDetail

--- app/src/main/java/com/example/questapi_116/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questapi_116/viewmodel/DetailViewModel.kt
@@ -1,2 +1,10 @@
 package com.example.questapi_116.viewmodel
 
+import com.example.questapi_116.modeldata.DataSiswa
+
+
+sealed interface StatusUIDetail {
+    data class Success(val satusiswa: DataSiswa) : StatusUIDetail
+    object Error : StatusUIDetail
+    object Loading : StatusUIDetail
+}
