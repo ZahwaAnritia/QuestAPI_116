a727a22ee406542ec07a75825587f6e03774f4d2
ZahwaAnritia <anritia15@gmail.com>
2025-12-25 16:16:59 +0700

    menambahkan composable DetailSiswaScreen

--- app/src/main/java/com/example/questapi_116/view/HalamanDetail.kt
+++ app/src/main/java/com/example/questapi_116/view/HalamanDetail.kt
@@ -1,2 +1,16 @@
 package com.example.questapi_116.view
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.questapi_116.viewmodel.DetailViewModel
+import com.example.questapi_116.viewmodel.provider.PenyediaViewModel
+
+
+@Composable
+fun DetailSiswaScreen(
+    navigateToEditItem: (Int) -> Unit,
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
