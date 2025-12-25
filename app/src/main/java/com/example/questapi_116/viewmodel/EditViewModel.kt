d9c43b9f3d427ccb55c0a7d1d1c59f6f3a299f7d
ZahwaAnritia <anritia15@gmail.com>
2025-12-25 15:47:10 +0700

    menambahkan import runtime

--- app/src/main/java/com/example/questapi_116/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/questapi_116/viewmodel/EditViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.questapi_116.viewmodel
 
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
