44e5fd7bd365abb2eef304f855cd4508baaa57fa
ZahwaAnritia <anritia15@gmail.com>
2025-12-25 15:39:42 +0700

    menambahkan DetailViewModel untuk detail siswa

--- app/src/main/java/com/example/questapi_116/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questapi_116/viewmodel/DetailViewModel.kt
@@ -1,6 +1,11 @@
 package com.example.questapi_116.viewmodel
 
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
 import com.example.questapi_116.modeldata.DataSiswa
+import com.example.questapi_116.repositori.RepositoryDataSiswa
 
 
 sealed interface StatusUIDetail {
@@ -8,3 +13,15 @@ sealed interface StatusUIDetail {
     object Error : StatusUIDetail
     object Loading : StatusUIDetail
 }
+
+class DetailViewModel(savedStateHandle: SavedStateHandle, private val repositoryDataSiswa:
+RepositoryDataSiswa
+): ViewModel()  {
+
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+    var statusUIDetail:StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
+        private set
+
+    init {
+        getSatuSiswa()
+    }
