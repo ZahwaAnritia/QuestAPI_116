699d0f5e88aaad361f3c6b193838e54c7ebb87c0
ZahwaAnritia <anritia15@gmail.com>
2025-12-25 15:45:15 +0700

    menambahkan load data siswa di EditViewModel

--- app/src/main/java/com/example/questapi_116/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/questapi_116/viewmodel/EditViewModel.kt
@@ -3,8 +3,10 @@ package com.example.questapi_116.viewmodel
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.questapi_116.modeldata.UIStateSiswa
 import com.example.questapi_116.repositori.RepositoryDataSiswa
+import kotlinx.coroutines.launch
 
 
 class EditViewModel(savedStateHandle: SavedStateHandle, private val repositoryDataSiswa:
@@ -12,3 +14,11 @@ RepositoryDataSiswa
 ): ViewModel() {
     var uiStateSiswa by mutableStateOf(UIStateSiswa())
         private set
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositoryDataSiswa.getSatuSiswa(idSiswa)
+                .toUiStateSiswa(true)
+        }
+    }
+
